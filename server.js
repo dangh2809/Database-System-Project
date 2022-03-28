d3e776b8ec7f82ed8fb6a2563408742df755e90d
Hieu Dang <dangh2809@gmail.com>
2022-03-28 15:01:52 -0400

    testing done, backend to database works, commented out the testing code

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const mysql = require("mysql");
 let port = process.env.PORT || 8080
 
@@ -18,25 +19,25 @@ if (process.env.NODE_ENV =="production")
 {
     app.use(express.static("./frontend/build"));
 }
-const testUser = {
-    firstName: "David",
-    lastName: "Blaine",
-    username: "DavidB",
-    password: "12345"
-}
-app.get("/test", (req, res) => {
-    console.log(
-        "server reach"
-    );
-    db.query("INSERT TO user (username, password, firstName, lastName) VALUES (?,?,?,?)", testUser.username, testUser.password, testUser.firstName, testUser.lastName, (err, result) => {
-        if (err){
-            console.log(err);
-        } else {
-            console.log("Data has been added");
-            res.send(result);
-        }
-    })
-})
+// const testUser = {
+//     firstName: "David",
+//     lastName: "Blaine",
+//     username: "DavidB",
+//     password: "12345"
+// }
+// app.get("/test", (req, res) => {
+//     console.log(
+//         "server reach"
+//     );
+//     db.query("INSERT INTO user (username, password, firstName, lastName) VALUES (?,?,?,?)", [testUser.username, testUser.password, testUser.firstName, testUser.lastName], (err, result) => {
+//         if (err){
+//             console.log(err);
+//         } else {
+//             console.log("Data has been added");
+//             res.send(result);
+//         }
+//     })
+// })
 app.listen( port, () =>{
     console.log(`Server is running on port ${port}`);
 })
