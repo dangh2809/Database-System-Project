ce9eefd32765c1482bf540e0dbc14164af16d1f7
Hieu Dang <dangh2809@gmail.com>
2022-03-28 11:56:15 -0400

    fix

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 const mysql = require("mysql");
+let port = process.env.PORT || 8080
 
 const db = mysql.createConnection({
     user:"root",
     host:"localhost",
-    password: process.env.MySQL_PASS,
+    password: "1234",
     database: "database-project"
 })
 
-app.listen(8080, () =>{
-    console.log("Server is running on port 8080");
+app.listen( port, () =>{
+    console.log(`Server is running on port ${port}`);
 })
